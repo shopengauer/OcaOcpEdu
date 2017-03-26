731e4b69e898dd488181c2e5d8f7b58b4a455ec9
root <localducks@gmail.com>
2017-03-26 22:14:29 +0300

    Add protected member

---
+++ src/accesslevel/secondpackage/Car.java
@@ -0,0 +1,32 @@
+package accesslevel.secondpackage;
+
+import accesslevel.Vechile;
+
+import java.time.LocalDate;
+
+/**
+ * Created by vasiliy on 26.03.17.
+ */
+public class Car extends Vechile {
+
+
+    private LocalDate localDate;
+
+    public String getColor(){
+        super.localDate = LocalDate.now();
+        // также имеем свой собственный localDate
+        this.localDate = LocalDate.now();
+        // наследуем protected член color класса Vechile
+        return color;
+    }
+
+    public Vechile createCar(){
+        Vechile vechile = new Vechile();
+        // но при создании класса Vechile protected члены не видны, они толко наследуются
+        vechile.localDate = null;
+        return vechile;
+
+    }
+
+
+}
