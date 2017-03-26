b39861f5697383cf9659168fb9f4633dd4855032
root <localducks@gmail.com>
2017-03-26 21:58:45 +0300

    Initial commit

---
+++ src/accesslevel/Vechile.java
@@ -0,0 +1,14 @@
+package accesslevel;
+
+import java.time.LocalDate;
+
+/**
+ * Created by vasiliy on 26.03.17.
+ */
+public class Vechile {
+
+    private String name;
+    protected String color;
+    public LocalDate localDate;
+
+}
